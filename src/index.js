3f9fbc39fa368bed109fdf741b6b7e265a103734
lidasharova <Lida050496@yandex.ru>
2023-01-19 21:22:58 +0700

    feat:complete task(9/11)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let BRACKETS_PAIR = Object.fromEntries(bracketsConfig);
+
+  let startBracket = '';
+  let endBracket = '';
+  for (brackets of bracketsConfig) {
+    startBracket += brackets[0];
+    endBracket += brackets[1];
+  }
+  let OPEN_BRACKETS = startBracket.split(''); //массив из открывающихся скобочек
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
