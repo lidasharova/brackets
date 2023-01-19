1f613d91ddf4232797c9dff05ba6eea87d1a26b1
lidasharova <Lida050496@yandex.ru>
2023-01-19 23:14:24 +0700

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // let BRACKETS_PAIR = Object.fromEntries(bracketsConfig); //не в той которая нужна , последовательности строится массив
-
   let BRACKETS_PAIR = {};
-  // let startBracket = '';
-  // let endBracket = '';
 
   // формируем объект пар закрывающая - открывающая
   for (let i = 0; i < bracketsConfig.length; i++) {
     BRACKETS_PAIR[bracketsConfig[i][1]] = bracketsConfig[i][0];
   }
 
-  // for (brackets of bracketsConfig) {
-  //   startBracket += brackets[0];
-  //   endBracket += brackets[1];
-  // }
-  // let OPEN_BRACKETS = startBracket.split(''); //массив из открывающихся скобочек
-
   let stack = [];
 
   for (let i = 0; i < str.length; i++) {
-    //находим символ что лежит сверху стека
+    //заводим переменную в которой лежит текущий знак
+    let current = str[i];
+    //находим символ, что лежит сверху стека
     let top = stack[stack.length - 1];
     //сравниваем символ из стека и символ по ключу объекта (значит они пара), так же проверяем длину стека
-    if (top === BRACKETS_PAIR[str[i]] && stack.length !== 0) {
-      stack.pop();   //удаляем символ из стека
+    if (BRACKETS_PAIR[current] === top && stack.length !== 0) {
+      stack.pop(); //удаляем этот символ
     } else {
-      stack.push(str[i]); //не совпадающие добавляем в стек
+      stack.push(current); //не совпадающие добавляем в стек
     }
   }
 
-  return stack.length === 0;
+  return stack.length === 0; //если в стеке что то есть - значит неккоректная последовательность
 };
+
+//методы которые не пригодились, но интересные
+//массив из открывающихся скобочек
+// let startBracket = '';
+// let endBracket = '';
+// for (brackets of bracketsConfig) {
+//   startBracket += brackets[0];
+//   endBracket += brackets[1];
+// }
+// let OPEN_BRACKETS = startBracket.split('');
+
+//как сделать объект из данного массива
+// let BRACKETS_PAIR = Object.fromEntries(bracketsConfig);
+//но не в той, последовательности, которая нужна, строится массив
